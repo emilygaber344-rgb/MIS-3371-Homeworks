b6990e03440c89593fd013f270efee0023138458
emilygaber344-rgb <emilygaber344@gmail.com>
2026-06-27 17:45:26 -0500

    please wok

--- MIS3371Brains.js
+++ MIS3371Brains.js
@@ -6,13 +6,7 @@ Name: Emily Gaber
  Purpose: To creat logic for the website to validate
  */
 
-// Credit to W3Schools!!!!! I altered it to fit my code but they produced the original.
 
-let UserPassword = document.getElementById("Password");
-//let SpecialChars = document.getElementById("SpecialChars");
-let capital = document.getElementById("capital");;
-let number = document.getElementById("number");
-let length = document.getElementById("length");
  
 // Credit to Technical Rajni and W3schools for the tutorial! I did change it however
 function ValidatePassword(){
@@ -43,93 +37,101 @@ function RealBirthdays() {
   const today = new Date();
 	  document.getElementById('birthday').setAttribute('max', today);
 }
-
+// Credit to W3Schools!!!!! I altered it to fit my code but they produced the original
 function Passwordmessage(){
-// When the user clicks on the password field, show the message box
-UserPassword.onfocus = function() {
-  document.getElementById("Passwordmessage").style.display = "block";
-}
-UserPassword.onblur = function() {
-  document.getElementById("Passwordmessage").style.display = "none";
-}
-// When the user starts to type something inside the password field
-UserPassword.onkeyup = function() {
-// Validate capital letters
-  let upperCaseLetters = /[A-Z]/g;
-  if(UserPassword.value.match(upperCaseLetters)) {  
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
+    let UserPassword = document.getElementById("Password");
+    let SpecialChars = document.getElementById("SpecialChars");
+    let capital = document.getElementById("capital");
+    let number = document.getElementById("number");
+    let length = document.getElementById("length");
+    // When the user clicks on the password field, show the message box
+    UserPassword.onfocus = function() {
+      document.getElementById("Passwordmessage").style.display = "block";
+    }
+    UserPassword.onblur = function() {
+      document.getElementById("Passwordmessage").style.display = "none";
+    }
+    // When the user starts to type something inside the password field
+    UserPassword.onkeyup = function() {
+    // Validate capital letters
+      let upperCaseLetters = /[A-Z]/g;
+      if(UserPassword.value.match(upperCaseLetters)) {  
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+      } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
 
-  let SpecialChars = /^(?=.*\d)(?!.*\s)/g;
-  if(UserPassword.value.match(SpecialChars)) {  
-    SpecialChars.classList.remove("invalid");
-    SpecialChars.classList.add("valid");
-  } else {
-    SpecialChars.classList.remove("valid");
-    SpecialChars.classList.add("invalid");
-  } 
-  // Validate numbers
-  let numbers = /[0-9]/g;
-  if(UserPassword.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
-  
-  // Validate length
-  if(UserPassword.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
-  }
-}
-}
-//Credit to Professor Jake! (i made some tweaks dont worry)
+      let SpecialChars = /^(?=.*\d)(?!.*\s)/g;
+      if(UserPassword.value.match(SpecialChars)) {  
+        SpecialChars.classList.remove("invalid");
+        SpecialChars.classList.add("valid");
+      } else {
+        SpecialChars.classList.remove("valid");
+        SpecialChars.classList.add("invalid");
+      } 
+      // Validate numbers
+      let numbers = /[0-9]/g;
+      if(UserPassword.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+      } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+      }
+      
+      // Validate length
+      if(UserPassword.value.length >= 8) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+      } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+      }
+    }
+    }
+    }
+//Credit to Professor Jake! (i made some tweaks dont worry! I must add that I used Google GEmini (You mentioned we can use it somewhat) to help me figure out why i coulnt get it to function)
 function returndata() {
-  
   let formcontents = document.getElementById("Intake");
+  let Review = document.getElementById("Review");
   let formoutput;
   let datatype;
   let i;
   Review.onclick = function() {
-  document.getElementById("extra").style.display = "block";
-  formoutput = "<table class='form'><th>Dataname</th><th>Value</th>";}
-  for (i = 0; i < formcontents.length; i++) {
-              datatype = formcontents.elements[i].type;
-              switch (datatype) {
-                case "checkbox":
-                  if (formcontents.elements[i].checked) {
-                    formoutput = formoutput + "<tr><td>"+formcontents.elements[i].name+"</td>";               
-                    formoutput = formoutput +"<td class='outputdata'>Checked</td></tr>";
-                  }
-                  break;
-               case "radio":
-                    if (formcontents.elements[i].checked) {
-                      formoutput = formoutput + "<tr><td >"+formcontents.elements[i].name+"</td>";
-                      formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
-                    }
-                  break;
-                case "button": case "submit": case "reset":
-                  formoutput = formoutput + "<tr><td>"+formcontents.elements[i].name+"</td>";
-                  formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
-                  break;
-                default:
-                  formoutput = formoutput + "<tr><td>"+formcontents.elements[i].name+"</td>";
-                  formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
-                }
-        
+    if (document.getElementById("returndata")) {
+      document.getElementById("returndata").style.display = "block";
+    }
 
-  }
+    formoutput = "<table class='form'><tr><th>Title</th><th>Input</th></tr>";
 
-   if (formoutput.length>0) { 
-      formoutput = formoutput + "</table>";
+    for (i = 0; i < formcontents.length; i++) {
+      datatype = formcontents.elements[i].type;
+
+      switch (datatype) {
+        case "checkbox":
+          if (formcontents.elements[i].checked) {
+            formoutput  = formoutput + "<tr><td>" + formcontents.elements[i].name + "</td>";               
+            formoutput  = formoutput + "<td class='returndata'>Checked</td></tr>";
+          }
+          break;
+
+        case "radio":
+          if (formcontents.elements[i].checked) {
+            formoutput  = formoutput + "<tr><td>" + formcontents.elements[i].name + "</td>";
+            formoutput  = formoutput + "<td class='returndata'>" + formcontents.elements[i].value + "</td></tr>";
+          }
+          break;
+        default:
+          formoutput  = formoutput + "<tr><td>" + formcontents.elements[i].name + "</td>";
+          formoutput  = formoutput + "<td class='returndata'>" + formcontents.elements[i].value + "</td></tr>";
+          break;
+      }
+    }
+    if (formoutput.length > 0) { 
+      formoutput  = formoutput + "</table>";
       document.getElementById("outputformdata").innerHTML = formoutput;
-   }
-}
+    }
+  };
+} 
+  returndata();
